5bb47a6b887477d45c680532946900659a6dc1df
Carl Boettiger <cboettig@gmail.com>
2015-04-23 12:44:24 -0400

    add additional recipes, tidy base recipe

--- bce-extensions/r-base-recipe.sh
+++ bce-extensions/r-base-recipe.sh
@@ -3,17 +3,14 @@ set -e
 
 apt-get update \
 	&& apt-get install -y --no-install-recommends \
-		ed \
-		less \
-		locales \
-		vim-tiny \
-		wget \
+		locales 
 
 ## Configure default locale, see https://github.com/rocker-org/rocker/issues/19
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen \
 	&& locale-gen en_US.utf8 \
 	&& /usr/sbin/update-locale LANG=en_US.UTF-8
 
+## Add mirrors to get the current version of R and the debian binaries for CRAN packages
 echo 'deb http://cran.rstudio.com/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list \
   && gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9 \
   && gpg -a --export E084DAB9 | sudo apt-key add - \
@@ -22,10 +19,24 @@ echo 'deb http://cran.rstudio.com/bin/linux/ubuntu trusty/' >> /etc/apt/sources.
 ## Set a default CRAN Repo
 echo 'options(repos = list(CRAN = "http://cran.rstudio.com/"))' >> /etc/R/Rprofile.site
 
-## Now install R and littler, and create a link for littler in /usr/local/bin
+## Now install R 
 apt-get update \
 	&& apt-get install -y --no-install-recommends \
+		ed \
+		less \
+    littler \
 		r-base=${R_BASE_VERSION}* \
 		r-base-dev=${R_BASE_VERSION}* \
 		r-recommended=${R_BASE_VERSION}* \
+		vim-tiny \
+		wget 
+
+## Configure littler helper scripts
+ln -s /usr/share/doc/littler/examples/install.r /usr/local/bin/install.r \
+	&& ln -s /usr/share/doc/littler/examples/install2.r /usr/local/bin/install2.r \
+	&& ln -s /usr/share/doc/littler/examples/installGithub.r /usr/local/bin/installGithub.r \
+	&& ln -s /usr/share/doc/littler/examples/testInstalled.r /usr/local/bin/testInstalled.r \
+  && r -e "install.packages('docopt', repos='http://cran.rstudio.com')" \
+	&& rm -rf /tmp/downloaded_packages/ /tmp/*.rds \
+	&& rm -rf /var/lib/apt/lists/*
 
